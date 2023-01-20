2378d139650760b8702c1c8d2b107ececaa6832c
Brishna Bakshev <b.bakshev@gmail.com>
2023-01-20 13:27:09 -0800

    added the code for the UI portion

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ function beepBoop(number) {
   let numArray = [];
   for (let i = 0; i <= number; i++) {
     if (i.toString() === "1") {
-      numArray.push("Beep!")
+        numArray.push("Beep!")
     } else if (i.toString() === "2") {
-      numArray.push("Boop!")
+        numArray.push("Boop!")
     } else if (i.toString() === "3") {
-      numArray.push("Won't you be my neighbor?")
+        numArray.push("Won't you be my neighbor?")
     } else {
-      numArray.push(i);
+        numArray.push(i);
     }
   }
   return numArray;
@@ -19,4 +19,14 @@ function beepBoop(number) {
 // UI Logic
 function handleForm(event) {
   event.preventDefault();
+
+  const input = document.getElementById("number").value;
+  const result = beepBoop(input);
+
+  document.getElementById("result").textContent = result;
 }
+
+
+window.addEventListener("load", function () {
+  document.getElementById("word-counter").addEventListener("submit", handleForm);
+});
