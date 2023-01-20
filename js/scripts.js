d1fdb6ae9bf3a7143b75b980b8da69db3367de7a
Brishna Bakshev <b.bakshev@gmail.com>
2023-01-20 09:56:29 -0800

    added a test and code to have the output return a string if number contains a 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 // Business Logic
 
-function beepBoop(number){
+function beepBoop(number) {
   let numArray = number.toString().split("");
-  for (let i = 0; i < numArray.length; i++){
+  for (let i = 1; i < numArray.length; i++) {
     if (numArray[i] === "1") {
       numArray[i] = "Beep!";
+    } else if (numArray[i] === "2") {
+      numArray[i] = "Boop!";
+    } else if (numArray[i] === "3") {
+      numArray[i] = "Won't you be my neighbor?";
     }
   }
   return numArray;
