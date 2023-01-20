9ab7d1cb9bdaf7b445adc9dee358b6b32ce7f41b
Brishna Bakshev <b.bakshev@gmail.com>
2023-01-20 11:18:07 -0800

    added an else statement so the output of numbers return the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 // Business Logic
 
 function beepBoop(number) {
-  let numArray = number.toString().split("");
-  for (let i = 1; i < numArray.length; i++) {
-    if (numArray[i] === "1") {
-      numArray[i] = "Beep!";
-    } else if (numArray[i] === "2") {
-      numArray[i] = "Boop!";
-    } else if (numArray[i] === "3") {
-      numArray[i] = "Won't you be my neighbor?";
+  let numArray = [];
+  for (let i = 0; i <= number; i++) {
+    if (i.toString() === "1") {
+      numArray.push("Beep!")
+    } else if (i.toString() === "2") {
+      numArray.push("Boop!")
+    } else if (i.toString() === "3") {
+      numArray.push("Won't you be my neighbor?")
+    } else {
+      numArray.push(i);
     }
   }
   return numArray;
