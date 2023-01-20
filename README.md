093375adaa95c3cfc558d49fa0f1619f0e2761b6
Brishna Bakshev <b.bakshev@gmail.com>
2023-01-20 14:17:03 -0800

    added a new test to replace the last digit of a number into a string and updated the js code to include the result

--- README.md
+++ README.md
@@ -2,18 +2,23 @@ Describe: beepBoop()
 
 Test: "It should return "Beep!" if a number entered contain a 1"
 Code:
-const number = 11;
+const number = 1;
 Expected Output: "Beep!"
 
 Test: "It should return "Boop!" if a number entered contain a 2"
 Code:
-const number = 12;
+const number = 2;
 Expected Output: "Boop!"
 
 Test: "It should return "Won't you be my neighbor?" if a number entered contain a 3
 Code:
-const number = 23;
+const number = 3;
 Expected Output: "Won't you be my neighbor?"
+
+Test: "It should display the remaiders of the inputed numbers"
+Code:
+const number = 12;
+Expected Output: [0, "Beep!", "Boop!", "Won't you be my neighbor?", 4, 5, 6, 7, 8, 9, "Beep!", "Boop!"]
 
 Test: "It should display a list of values starting with 0 + user's inputed number"
 Code:
--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,18 @@
 function beepBoop(number) {
   let numArray = [];
   for (let i = 0; i <= number; i++) {
-    if (i.toString() === "1") {
-        numArray.push("Beep!")
-    } else if (i.toString() === "2") {
-        numArray.push("Boop!")
-    } else if (i.toString() === "3") {
-        numArray.push("Won't you be my neighbor?")
-    } else {
-        numArray.push(i);
+    let lastDigit = i % 10;
+        if(lastDigit === 1) {
+            numArray.push("Beep!");
+        } else if(lastDigit === 2) {
+            numArray.push("Boop!");
+        } else if(lastDigit === 3) {
+            numArray.push("Won't you be my neighbor?");
+        } else {
+            numArray.push(i);
+        }
     }
-  }
-  return numArray;
+    return numArray;
 }
 
 // UI Logic
@@ -23,7 +24,7 @@ function handleForm(event) {
   const input = document.getElementById("number").value;
   const result = beepBoop(input);
 
-  document.getElementById("result").textContent = result;
+  document.getElementById("result").innerText = result;
 }
 
 
